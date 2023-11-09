7fbd1ee5ebd454739fafa53eab2160e84a606512
Ardiyansyah R <arsk9797@gmail.com>
2023-11-09 23:48:39 +0800

    membuat service section

---
+++ src/components/Service.jsx
@@ -0,0 +1,49 @@
+export default function Service() {
+  return (
+    <>
+      <section className="services-section my-5 mx-3" id="services-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5 offset-md-1 col-sm-12 p-3 serv-img">
+              <img src="images/img_service.png" width="100%" alt="Services" />
+            </div>
+
+            <div className="col-md-5 col-sm-12 my-auto">
+              <h2 className="h2">
+                Best Car Rental for any kind of trip in (Lokasimu)!
+              </h2>
+              <p className="teks-service">
+                Sewa mobil di (Lokasimu) bersama Binar Car Rental jaminan harga
+                lebih murah dibandingkan yang lain, kondisi mobil baru, serta
+                kualitas pelayanan terbaik untuk perjalanan wisata, bisnis,
+                wedding, meeting, dll.
+              </p>
+              <ul className="list-service">
+                <li>
+                  <img src="images/Group_53.svg" width="24" alt="" />
+                  Sewa Mobil Dengan Supir di Bali 12 Jam
+                </li>
+                <li>
+                  <img src="images/Group_53.svg" width="24" alt="" />
+                  Sewa Mobil Lepas Kunci di Bali 24 Jam
+                </li>
+                <li>
+                  <img src="images/Group_53.svg" width="24" alt="" />
+                  Sewa Mobil Jangka Panjang Bulanan
+                </li>
+                <li>
+                  <img src="images/Group_53.svg" width="24" alt="" />
+                  Gratis Antar - Jemput Mobil di Bandara
+                </li>
+                <li>
+                  <img src="images/Group_53.svg" width="24" alt="" />
+                  Layanan Airport Transfer / Drop In Out
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Navigation from "../components/Navbar";
+import Service from "../components/Service";
 
 export default function LandingPage() {
   return (
     <>
       <Navigation />
       <Hero />
+      <Service />
     </>
   );
 }
