84eb9fa265dd4247311e70edf5300a6bc660cc3a
Ardiyansyah R <arsk9797@gmail.com>
2023-11-10 01:02:41 +0800

    membuat banner section

---
+++ src/components/Banner.jsx
@@ -0,0 +1,22 @@
+export default function Banner() {
+  return (
+    <>
+      <section className="banner-section my-5 mx-3" id="banner-section">
+        <div className="container text-center banner">
+          <div className="row">
+            <div className="col-md-12 col-sm-12 my-5">
+              <h1 className="h1 mb-3">Sewa Mobil di (Lokasimu) Sekarang</h1>
+              <p className="sub-title mx-auto">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <button className="btn-sewa fw-bold my-3">
+                Mulai Sewa Mobil
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
